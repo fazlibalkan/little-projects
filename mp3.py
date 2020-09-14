1f376d1a29c0116f917544817d795dc454566ed8
fazlibalkan <53044560+fazlibalkan@users.noreply.github.com>
2020-09-14 16:40:43 +0300

    pause, unpause, volume

--- mp3.py
+++ mp3.py
@@ -13,6 +13,10 @@ os.chdir("D:/mp3-player/mp3-player/Songs")
 print(os.getcwd)
 songlist = os.listdir()
 
+
+"""Volume"""
+VolumeLevel = tkr.Scale(player, from_= 0.0, to_=1.0, orient = tkr.HORIZONTAL, resolution = 0.1)
+
 """Playlist"""
 playlist = tkr.Listbox(player, highlightcolor = "blue", selectmode = tkr.SINGLE)
 print(songlist)
@@ -32,15 +36,25 @@ def Play():
 	pygame.mixer.music.load(playlist.get(tkr.ACTIVE))
 	var.set(playlist.get(tkr.ACTIVE))
 	pygame.mixer.music.play()
+	pygame.mixer.music.set_volume(VolumeLevel.get())
 
 def ExitPlayer():
 	pygame.mixer.music.stop()
 
+def Pause():
+	pygame.mixer.music.pause()
+
+def Unpause():
+	pygame.mixer.music.unpause()
 
 
 """Buttons"""
 button1 = tkr.Button(player, width=5, height=3, text="PLAY", command=Play)
 button2 = tkr.Button(player, width=5, height=3, text="STOP", command=ExitPlayer)
+button3 = tkr.Button(player, width=5, height=3, text="PAUSE", command=Pause)
+button4 = tkr.Button(player, width=5, height=3, text="UNPAUSE", command=Unpause)
+
+
 
 """Songs"""
 var = tkr.StringVar()
@@ -51,6 +65,9 @@ songtitle = tkr.Label(player, textvariable = var)
 """Place Widgets"""
 button1.pack(fill="x")
 button2.pack(fill="x")
+button3.pack(fill="x")
+button4.pack(fill="x")
+VolumeLevel.pack(fill="x")
 songtitle.pack()
 playlist.pack(fill="both", expand= "yes")
 
