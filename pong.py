1007d0270ae1873ed20619b429ae5f14b56a2a53
fazlibalkan <53044560+fazlibalkan@users.noreply.github.com>
2020-09-16 21:07:11 +0300

    score

--- pong.py
+++ pong.py
@@ -7,6 +7,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# score
+score_a = 0
+score_b = 0
+
 # paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -35,6 +39,15 @@ ball.goto(0, 0)
 ball.dx = 0.15
 ball.dy = 0.15
 
+#pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 
 """Functions"""
 def PaddleAUp():
@@ -51,11 +64,13 @@ def PaddleBUp():
 	y = paddle_b.ycor()
 	y += 20
 	paddle_b.sety(y)
+	
 
 def PaddleBDown():
 	y = paddle_b.ycor()
 	y -= 20
 	paddle_b.sety(y)
+	
 
 
 """Keybord Binding"""
@@ -85,9 +100,16 @@ while True:
 	if ball.xcor() > 390:
 		ball.goto(0, 0)
 		ball.dx *= -1
+		score_a += 1
+		pen.clear()
+		pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 	if ball.xcor() < -390:
 		ball.goto(0, 0)
 		ball.dx *= -1
+		score_b += 1
+		pen.clear()
+		pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
 
 	#paddle and ball collision
 	if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() -50):
